caa6d29bf7e7a859e94355d13a1fbc8baea7f3e2
Pradeep Kumar Tippa <pktippa@gmail.com>
2020-12-14 15:07:45 +0530

    more examples on frequency counter

--- javascript/patterns/frequency_counter.js
+++ javascript/patterns/frequency_counter.js
@@ -26,4 +26,44 @@ function same(arr1, arr2){
     return true
 }
 
-console.log(same([1,2,3,2,5], [9,1,4,4,25]))
+// console.log(same([1,2,3,2,5], [9,1,4,4,25]))
+
+function sameFrequency(first, second){
+    // good luck. Add any arguments you deem necessary.
+    let f = {};
+    while(first > 0) {
+        const key = first % 10;
+        // console.log('key: ', key);
+        f[key] = (f[key] || 0) +1;
+        first =  (first / 10) - (0.1 * key) ;
+        // console.log('first: ', first);
+    }
+    // console.log('f', f);
+    while(second > 0) {
+        const key = Math.ceil(second % 10);
+        if(!f[key] || f[key] == 0) {
+            return false;
+        }
+        f[key] -= 1;
+        second = (second / 10) - (0.1 * key) ;
+        // console.log('f', f, ' sec ', second);
+    }
+    return true;
+}
+// console.log(sameFrequency(182, 281));
+// console.log(sameFrequency(34, 14));
+// console.log(sameFrequency(3589578, 5879385));
+// console.log(sameFrequency(22, 222));
+
+function areThereDuplicates(...arr) {
+    // good luck. (supply any arguments you deem necessary.)
+    let obj = {};
+    for(const el of arr) {
+        if(obj[el]) return false;
+        obj[el] = (obj[el] && obj[el] + 1) || 1;
+    }
+  }
+
+// console.log(areThereDuplicates(1,2,3)); // false
+// console.log(areThereDuplicates(1,2,2)); // true
+// console.log(areThereDuplicates('a','b','c', 'a')); // true

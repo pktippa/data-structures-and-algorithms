0dc91cd3a8b43ba173324b7111e5cdee797ef5fb
Pradeep Kumar Tippa <pktippa@gmail.com>
2020-12-19 17:33:25 +0530

    added more methods on singly linked list

--- javascript/singly_linked_list.js
+++ javascript/singly_linked_list.js
@@ -5,101 +5,157 @@
 // it has length property
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
 class SinglyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  /**
+   * if this is first element (both head and tail are null)
+   * set both head and tail to first element.
+   *
+   * if it is second or next time.
+   *
+   * @param {*} val Node
+   */
+  push(val) {
+    const n = new Node(val);
+    if (!this.head && !this.tail) {
+      this.head = n;
+      this.tail = n;
+    } else {
+      this.tail.next = n;
+      this.tail = n;
     }
-    /**
-     * if this is first element (both head and tail are null)
-     * set both head and tail to first element.
-     * 
-     * if it is second or next time.
-     * 
-     * @param {*} val Node
-     */
-    push(val) {
-        const n = new Node(val);
-        if(!this.head && !this.tail) {
-            this.head = n;
-            this.tail = n;
-        } else {
-            this.tail.next = n;
-            this.tail = n;
-        }
-        this.length += 1;
-        return this;
-    }
+    this.length += 1;
+    return this;
+  }
 
-    pop() {
-        if(!this.head) return null;
-        if(!this.head.next) {
-            const item = this.head;
-            this.head = null;
-            this.tail = null;
-            this.length--;
-            return item;
-        }
-        let current = this.head;
-        let newTail = current;
-        while(current.next) {
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        return current;
-    }   
-    
-    shift(){
-        if(!this.head) return undefined;
-        var currentHead = this.head;
-        this.head = currentHead.next;
-        this.length--;
-        if(this.length === 0){
-            this.tail = null;
-        }
-        return currentHead;
+  pop() {
+    if (!this.head) return null;
+    if (!this.head.next) {
+      const item = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return item;
     }
-    
-    unshift(val) {
-        const n = new Node(val);
-        if(!this.head){
-            this.head = n;
-            this.tail = n;
-            this.length++;
-            return this;
-        }
-        const currentHead = this.head;
-        this.head = n;
-        this.head.next = currentHead;
-        this.length++;
-        return this;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    return current;
+  }
 
-    get(index) {
-        if(index <0 || index >= this.length) return null;
-        let counter = 0;
-        let current = this.head;
-        while(counter !== index){
-            current = current.next;
-            counter++;
-        }
-        return current;
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
     }
+    return currentHead;
+  }
 
-    set(index, val) {
-        let node = this.get(index);
-        if(!node) return false;
-        node.val = val;
-        return true;
+  unshift(val) {
+    const n = new Node(val);
+    if (!this.head) {
+      this.head = n;
+      this.tail = n;
+      this.length++;
+      return this;
     }
+    const currentHead = this.head;
+    this.head = n;
+    this.head.next = currentHead;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (!node) return false;
+    node.val = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) {
+      this.unshift(val);
+
+      return true;
+    }
+    if (index === this.length) {
+      this.push(val);
+      return true;
+    }
+    const newNode = new Node(val);
+    const foundNode = this.get(index - 1);
+    const prevNext = foundNode.next;
+    foundNode.next = newNode;
+    newNode.next = prevNext;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    const prvNode = this.get(index - 1);
+    const afterNode = prvNode.next.next;
+    const returnNode = prvNode.next;
+    prvNode.next = afterNode;
+    this.length--;
+    return returnNode;
+  }
+
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
 }

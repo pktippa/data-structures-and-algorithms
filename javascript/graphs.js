c98d191eed7db31ca8aa9eca6b80dc990dbe1196
Pradeep Kumar Tippa <pktippa@gmail.com>
2021-03-15 16:58:12 +0530

    graph remove edge and vertex

--- javascript/graphs.js
+++ javascript/graphs.js
@@ -4,6 +4,7 @@ class Graph {
     constructor() {
         this.adjacentList = {};
     }
+    // Vertex is nothing but Node
     // Add a vertex 
     // 1. accepts a name of the vertex
     // 2. add a key to the adjacent list with the name of the vertex and sets its
@@ -27,7 +28,34 @@ class Graph {
             this.adjacentList[vertex2].push(vertex1);
         }
     }
-
+    // the function should reassign the key of vertex1 to be an array
+    // that does not contain vertex2
+    // the function should reassign the key of vertex2 to be an array
+    // that does not contain vertex1
     removeEdge(vertex1, vertex2) {
+        // const idx = this.adjacentList[vertex1].indexOf(vertex2);
+        // this.adjacentList[vertex1].splice(idx, 1);
+
+        // const idx2 = this.adjacentList[vertex2].indexOf(vertex1);
+        // this.adjacentList[vertex2].splice(idx2, 1);
+
+        // Alternative implementation, filter not required ones
+        this.adjacentList[vertex1] = this.adjacentList[vertex1].filter(vertex => vertex !== vertex2);
+        this.adjacentList[vertex2] = this.adjacentList[vertex2].filter(vertex => vertex !== vertex1);
+    }
+
+    // My intuition:
+    // vertex can be part of edges
+    // so if we remove a vertex, loop through all the other vertices that are connected through edges
+    // and for each of these edges call removeEdge
+
+    // From course:
+    removeVertex(name) {
+        if(this.adjacentList[name]) {
+            for(const vertex of this.adjacentList[name]) {
+                this.removeEdge(name, vertex);
+            }
+            delete this.adjacentList[name];
+        }
     }
 }

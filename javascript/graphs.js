f3ef1ecbcbe495d6a71d7e4724bbda6de1eae5aa
Pradeep Kumar Tippa <pktippa@gmail.com>
2021-03-15 18:01:43 +0530

    graph dfs recursive approach

--- javascript/graphs.js
+++ javascript/graphs.js
@@ -10,7 +10,7 @@ class Graph {
     // 2. add a key to the adjacent list with the name of the vertex and sets its
     //    value to the empty array
     addVertex(name) {
-        if(!this.adjacentList[name]) {
+        if (!this.adjacentList[name]) {
             this.adjacentList[name] = [];
         }
     }
@@ -51,11 +51,82 @@ class Graph {
 
     // From course:
     removeVertex(name) {
-        if(this.adjacentList[name]) {
-            for(const vertex of this.adjacentList[name]) {
+        if (this.adjacentList[name]) {
+            for (const vertex of this.adjacentList[name]) {
                 this.removeEdge(name, vertex);
             }
             delete this.adjacentList[name];
         }
     }
+
+    /**
+     *  DFS(vertex):
+        if vertex is empty
+            return (this is base case)
+        add vertex to results list
+        mark vertex as visited
+        for each neighbor in vertex's neighbors:
+        if neighbor is not visited:
+          recursively call DFS on neighbor
+     * 
+     * 
+     * 
+     * The function should accept a starting node
+
+        Create a list to store the end result, to be returned at the very end
+
+        Create an object to store visited vertices
+
+        Create a helper function which accepts a vertex
+
+        The helper function should return early if the vertex is empty
+
+        The helper function should place the vertex it accepts into the visited object and push that vertex into the result array.
+
+        Loop over all of the values in the adjacencyList for that vertex
+
+        If any of those values have not been visited, recursively invoke the helper function with that vertex
+
+        Invoke the helper function with the starting vertex
+
+        Return the result array
+     * 
+     * @param {*} vertex 
+     */
+
+    DFS_Recursive(vertex) {
+        let finalResult = [];
+        let visited = {};
+        const _this = this;
+        function traverse(vertex) {
+            if (_this.adjacentList[vertex].length === 0) {
+                return;
+            }
+            visited[vertex] = true;
+            finalResult.push(vertex);
+            for (const v of _this.adjacentList[vertex]) {
+                if (!visited[v]) {
+                    traverse(v);
+                }
+            }
+        }
+        traverse(vertex);
+
+        return finalResult;
+    }
 }
+const g = new Graph();
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+g.addEdge("A","B")
+g.addEdge("A","C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
